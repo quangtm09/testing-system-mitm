38020520f4d499cda6e3787e27913d442d28fad5
loanhuynh89@gmail.com <loanhuynh89@gmail.com@c76654c6-2bb8-a27b-3455-f733d6d83e75>
2014-03-15 09:12:55 +0000

    [LHUYNH] Define constant

---
+++ TestingSystem/src/data/constant/PermissionConstant.java
@@ -0,0 +1,51 @@
+package data.constant;
+
+public class PermissionConstant {
+	/* Create Test Permission */
+	public static final int CREATE_TEST = 1;
+	public static final String str_CREATE_TEST = "Create Test";
+	
+	// Edit Test Permission
+	public static final int EDIT_TEST = 2;
+	public static final String str_EDIT_TEST = "Edit Test";
+	
+	// Do Test Permission
+	public static final int DO_TEST = 3;
+	public static final String str_DO_TEST = "Taking Test";
+	
+	// VIew Test Permission
+	public static final int VIEW_TEST = 4;
+	public static final String str_VIEW_TEST = "View Test";
+	
+	// Delete Test Permission
+	public static final int DELETE_TEST = 5;
+	public static final String str_DELETE_TEST = "Delete Test";
+	
+	// View Test Result Permission
+	public static final int VIEW_RESULTS = 6;
+	public static final String str_VIEW_RESULTS = "View Result";
+	
+	// Create Account Permission
+	public static final int CREATE_ACCOUNT = 7;
+	public static final String str_CREATE_ACCOUNT = "Create Account";
+	
+	// Add User Permission
+	public static final int ADD_USER = 8;
+	public static final String str_ADD_USER = "Add User";
+	
+	//Update Profile Permission
+	public static final int UPDATE_PROFILE = 9;
+	public static final String str_UPDATE_PROFILE = "Update Profile";
+	
+	// View History Permission
+	public static final int VIEW_HISTORY = 10;
+	public static final String str_VIEW_HISTORY = "View History";
+	
+	// Manage User Permission
+	public static final int MANAGE = 11;
+	public static final String str_MANAGE = "Manage User";
+	
+	// Remove User Permission
+	public static final int REMOVE_USER = 12;
+	public static final String str_REMOVE_USER = "Remove User";
+}
