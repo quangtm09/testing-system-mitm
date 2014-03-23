a709d4c2d40996211729a059ce162a9e994b5ff2
loanhuynh89@gmail.com <loanhuynh89@gmail.com@c76654c6-2bb8-a27b-3455-f733d6d83e75>
2014-03-23 12:22:20 +0000

    Modify Configuaration to AnnotationConfiguration

--- TestingSystem/src/util/HibernateUtil.java
+++ TestingSystem/src/util/HibernateUtil.java
@@ -1,24 +1,38 @@
 package util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.AnnotationConfiguration;
+//import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	private static final SessionFactory sessionFactory;
-	static{
-		try{
-			sessionFactory=new Configuration().configure().buildSessionFactory();
-			
-		}catch(Exception e){
-			e.printStackTrace();
-			System.err.println("Can't init session Factory");
-			throw new ExceptionInInitializerError(e);
-			
-		}
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+	 
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new AnnotationConfiguration().configure().buildSessionFactory();
+ 
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+ 
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+ 
+    public static void shutdown() {
+    	// Close caches and connection pools
+    	getSessionFactory().close();
+    }
+
+	public static void closeSession() {
+		// TODO Auto-generated method stub
+		sessionFactory.getCurrentSession().close();
 		
 	}
-	public static SessionFactory getSessionFactory(){
-		return sessionFactory;
-	}
-
+ 
 }
