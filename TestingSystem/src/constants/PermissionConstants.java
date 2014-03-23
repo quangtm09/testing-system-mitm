a708e3c9734558d378543311a18409dc2f07db30
loanhuynh89@gmail.com <loanhuynh89@gmail.com@c76654c6-2bb8-a27b-3455-f733d6d83e75>
2014-03-23 18:24:58 +0000

    Add more permissions

--- TestingSystem/src/constants/PermissionConstants.java
+++ TestingSystem/src/constants/PermissionConstants.java
@@ -10,8 +10,8 @@ public class PermissionConstants {
 	public static final String str_EDIT_TEST = "Edit Test";
 
 	// Do Test Permission
-	public static final int DO_TEST = 3;
-	public static final String str_DO_TEST = "Taking Test";
+	public static final int TAKE_TEST = 3;
+	public static final String str_TAKE_TEST = "Taking Test";
 
 	// VIew Test Permission
 	public static final int VIEW_TEST = 4;
@@ -33,7 +33,7 @@ public class PermissionConstants {
 	public static final int ADD_USER = 8;
 	public static final String str_ADD_USER = "Add User";
 
-	//Update Profile Permission
+	// Update Profile Permission
 	public static final int UPDATE_PROFILE = 9;
 	public static final String str_UPDATE_PROFILE = "Update Profile";
 
@@ -48,4 +48,36 @@ public class PermissionConstants {
 	// Remove User Permission
 	public static final int REMOVE_USER = 12;
 	public static final String str_REMOVE_USER = "Remove User";
+
+	// Update Test
+	public static final int UPDATE_TEST = 13;
+	public static final String str_UPDATE_TEST = "Update Test";
+
+	// Deploy Test
+	public static final int DEPLOY_TEST = 14;
+	public static final String str_DEPLOY_TEST = "Deploy Test";
+
+	// View Account
+	public static final int VIEW_ACCOUNT = 15;
+	public static final String str_VIEW_ACCOUNT = "View Account";
+	
+	// Drop Account
+	public static final int DROP_ACCOUNT = 16;
+	public static final String str_DROP_ACCOUNT = "Drop Account";
+	
+	//Create Question
+	public static final int CREATE_QUESTION = 17;
+	public static final String str_CREATE_QUESTION = "Create Question";
+	
+	// View Question
+	public static final int VIEW_QUESTION = 18;
+	public static final String str_VIEW_QUESTION = "View Question";
+	
+	// Update Question
+	public static final int UPDATE_QUESTION = 19;
+	public static final String str_UPDATE_QUESTION = "Update Question";
+	
+	// Drop Question
+	public static final int DROP_QUESTION = 20;
+	public static final String str_DROP_QUESTION = "Drop Question";
 }
